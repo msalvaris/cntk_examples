db383c90a1f88f42e2b5c0cabb14d9cf81e63407
MSalvaris <msalvaris@users.noreply.github.com>
2017-06-07 20:03:58 +0100

    Changes newline CRLF to LF

--- convert_cifar10.sh
+++ convert_cifar10.sh
@@ -8,4 +8,4 @@ CIFAR_DATA=$2
 
 BASEDIR=$(pwd)
 
-docker run --rm -v $CIFAR_DATA:$CIFAR_DATA -w $CIFAR_DATA -v $BASEDIR:/code $IMAGE_NAME /bin/bash -c "source /cntk/activate-cntk; python /code/cifar_data_processing.py --datadir $CIFAR_DATA"
+docker run --rm -v $CIFAR_DATA:$CIFAR_DATA -w $CIFAR_DATA -v $BASEDIR:/code $IMAGE_NAME /bin/bash -c "source /cntk/activate-cntk; python -u /code/cifar_data_processing.py --datadir $CIFAR_DATA"

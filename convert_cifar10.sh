6747fe407d761bf803f41a395f17d97aa3e5d7ad
MSalvaris <msalvaris@users.noreply.github.com>
2017-06-07 12:27:51 +0100

    Adds local directory to docker container

--- convert_cifar10.sh
+++ convert_cifar10.sh
@@ -6,4 +6,6 @@ set -o pipefail
 IMAGE_NAME=$1
 CIFAR_DATA=$2
 
-docker run --rm -v $CIFAR_DATA:$CIFAR_DATA -w $CIFAR_DATA $IMAGE_NAME /bin/bash -c "source /cntk/activate-cntk; python cifar_data_processing.py --datadir $CIFAR_DATA"
+BASEDIR=$(pwd)
+
+docker run --rm -v $CIFAR_DATA:$CIFAR_DATA -w $CIFAR_DATA -v $BASEDIR:/code $IMAGE_NAME /bin/bash -c "source /cntk/activate-cntk; python /code/cifar_data_processing.py --datadir $CIFAR_DATA"
